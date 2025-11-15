16fdbb828f5aa3e51d53c4e676f129d6ae8317bb
Luke Wren <wren6991@gmail.com>
2025-11-15 08:04:43 +0000

    Remove need to source project_env.sh (still need to be checked out inside Hazard3 repository

--- cores/hazard3/generate.sh
+++ cores/hazard3/generate.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -ex
+
+# Assuming checked out inside of Hazard3 repository
+export PROJ_ROOT=$(realpath ../../../../../..)
+export HDL=${PROJ_ROOT}/hdl
+
 TB_ROOT=$PROJ_ROOT/test/formal/riscv-formal/tb
+LISTFILES=$PROJ_ROOT/scripts/listfiles
+
+export 
 
 rm -rf hdl
 mkdir hdl
-cp $(listfiles $TB_ROOT/hazard3_rvfi.f) hdl
-for incdir in $(listfiles -f flati $TB_ROOT/hazard3_rvfi.f); do
+cp $(${LISTFILES} $TB_ROOT/hazard3_rvfi.f) hdl
+for incdir in $(${LISTFILES} -f flati $TB_ROOT/hazard3_rvfi.f); do
 	cp $incdir/*.vh hdl
 done
 
